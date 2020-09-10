ffd7e77bbd8cda4dab08a13b607fc36b736f7b9d
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2020-09-10 17:49:04 +0530

    svelte ie11 working code

--- svelte-first-steps/rollup.config.js
+++ svelte-first-steps/rollup.config.js
@@ -3,6 +3,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import babel from 'rollup-plugin-babel';
+//import "@babel/polyfill";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -35,6 +37,22 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
+		babel({
+			extensions: ['.js', '.mjs', '.html', '.svelte'],
+			runtimeHelpers: true,
+			exclude: ['node_modules/@babel/**'],
+			presets: [
+				['@babel/preset-env', {
+					targets: '> 0.25%, not dead'
+				}]
+			],
+			plugins: [
+				'@babel/plugin-syntax-dynamic-import',
+				['@babel/plugin-transform-runtime', {
+					useESModules: true
+				}]
+			]
+		}),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
